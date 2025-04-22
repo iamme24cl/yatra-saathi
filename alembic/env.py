71610058c262c0ada701a39281f5e8069620cb1c
Chandra Lama <iamme24cl@gmail.com>
2025-04-21 21:57:19 -0700

    remove alembic/env from version control

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import create_engine
 import os
-from app.database import Base
+from app.core.database import Base
 from dotenv import load_dotenv
 
 from alembic import context
@@ -17,18 +17,24 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+    
+# Add app's path so imports work
+import sys
+sys.path.append(os.getcwd())
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from app.core.database import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+from app.core.config import settings
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
